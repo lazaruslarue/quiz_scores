6ac0edbccb87f30e4e92e104ce5d11536a9520fb
lazaruslarue <lazaruslarue@gmail.com>
2016-09-04 17:32:31 -0600

    cleanup

--- src/Components/Quiz/ScoreList/scorelist.controller.js
+++ src/Components/Quiz/ScoreList/scorelist.controller.js
@@ -3,16 +3,12 @@ class ScorelistController {
   $onInit() {
     this.scores =  this.QuizCtrl.getScores()
   }
-  $onChanges(changes) {
-
-  }
+  $onChanges(changes) {  }
   deleteScore(score) {
     var idx = this.scores.indexOf(score)
     this.scores.splice(idx, 1)
     this.QuizCtrl.putScores(this.scores)
   }
 }
-
-ScorelistController.$inject = ['QuizService']
 
 export default ScorelistController
