4bbe6ec5dd5594632cd4da3af5ec098320d89cbd
lazaruslarue <lazaruslarue@gmail.com>
2016-09-08 19:06:06 -0600

    app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
-import angular from 'angular'
-import angularCookies   from 'angular-cookies'
 import AppComponent from './app.component'
 import Components from './Components'
 
-
-
 angular
   .module('app', [
-    angularCookies,
     Components
   ])
   .component('app', AppComponent)
